2a16555363c516d1331dec40bcb4134747a98ca7
MaHieu02 <21021657@vnu.edu.vn>
2025-05-07 22:55:26 +0700

    update

--- app/src/main/java/com/example/hellothegioi/data/repository/ExampleNotification.kt
+++ app/src/main/java/com/example/hellothegioi/data/repository/ExampleNotification.kt
@@ -3,20 +3,15 @@ package com.example.hellothegioi.data.repository
 import com.example.hellothegioi.data.model.Notification
 import com.example.hellothegioi.data.model.NotificationTarget
 import com.example.hellothegioi.data.model.NotificationTarget.ComposePage
-import java.util.Arrays
 
 
 class ExampleNotification {
     var lispots = ExamplePost.getAll()
 
-    var notifications: List<Notification> = Arrays.asList(
-        Notification("Bạn có một bài tập mới.", "10:00 AM", NotificationTarget.Post(lispots[1])),
-        Notification("Lịch học ngày mai đã được cập nhật.", "11:00 AM", NotificationTarget.Post(lispots[2])),
-        Notification(
-            "Giảng viên đã chấm điểm bài kiểm tra.",
-            "12:00 PM",
-            NotificationTarget.Post(lispots[2])
-        ),
-        Notification("Giảng viên đã chấm điểm bài kiểm tra.", "12:00 PM", ComposePage("question"))
-    )
+    var notificationsList = ArrayList<Notification>().apply {
+        add(Notification("Bài viết của bạn vừa nhận được 1 bình luận.", "10:00 AM", NotificationTarget.Post(lispots[1])))
+        add(Notification("Bài viết bạn theo dõi vừa nhận được một bình luận.", "11:00 AM", NotificationTarget.Post(lispots[2])))
+        add(Notification("Bài viết bạn theo dõi vừa nhận được 2 bình luận.", "12:00 PM", NotificationTarget.Post(lispots[2])))
+        add(Notification("Bạn có muốn thực hiện câu hỏi hằng ngày không", "12:00 PM", ComposePage("question")))
+    }
 }
